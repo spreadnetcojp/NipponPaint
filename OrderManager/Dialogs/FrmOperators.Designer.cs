08e337c05eeb1ab2096d0c4755b12dc41fcef0d5
YuyaMieda <100652769+YuyaMieda@users.noreply.github.com>
2022-06-13 11:06:40 +0900

    担当者を決定ダイアログ作成
    担当者を決定ダイアログ作成

---
+++ OrderManager/Dialogs/FrmOperators.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace NipponPaint.OrderManager.Dialogs
+{
+    partial class FrmOperators
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.DgvListLeft = new System.Windows.Forms.DataGridView();
+            this.PnlBtn = new System.Windows.Forms.Panel();
+            this.BtnAssign = new System.Windows.Forms.Button();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.DgvListRight = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvListLeft)).BeginInit();
+            this.PnlBtn.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvListRight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvListLeft
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgvListLeft.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.DgvListLeft.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DgvListLeft.DefaultCellStyle = dataGridViewCellStyle2;
+            this.DgvListLeft.Location = new System.Drawing.Point(12, 12);
+            this.DgvListLeft.Name = "DgvListLeft";
+            this.DgvListLeft.RowTemplate.Height = 21;
+            this.DgvListLeft.Size = new System.Drawing.Size(480, 492);
+            this.DgvListLeft.TabIndex = 30;
+            // 
+            // PnlBtn
+            // 
+            this.PnlBtn.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.PnlBtn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PnlBtn.Controls.Add(this.BtnClose);
+            this.PnlBtn.Controls.Add(this.BtnAssign);
+            this.PnlBtn.Location = new System.Drawing.Point(252, 510);
+            this.PnlBtn.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.PnlBtn.Name = "PnlBtn";
+            this.PnlBtn.Size = new System.Drawing.Size(496, 71);
+            this.PnlBtn.TabIndex = 92;
+            // 
+            // BtnAssign
+            // 
+            this.BtnAssign.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtnAssign.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.BtnAssign.Location = new System.Drawing.Point(19, 7);
+            this.BtnAssign.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.BtnAssign.Name = "BtnAssign";
+            this.BtnAssign.Size = new System.Drawing.Size(220, 54);
+            this.BtnAssign.TabIndex = 2;
+            this.BtnAssign.Text = "担当者を決定";
+            this.BtnAssign.UseVisualStyleBackColor = true;
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtnClose.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.BtnClose.Location = new System.Drawing.Point(256, 7);
+            this.BtnClose.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(220, 54);
+            this.BtnClose.TabIndex = 93;
+            this.BtnClose.Text = "閉じる";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            // 
+            // DgvListRight
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgvListRight.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.DgvListRight.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DgvListRight.DefaultCellStyle = dataGridViewCellStyle4;
+            this.DgvListRight.Location = new System.Drawing.Point(509, 12);
+            this.DgvListRight.Name = "DgvListRight";
+            this.DgvListRight.RowTemplate.Height = 21;
+            this.DgvListRight.Size = new System.Drawing.Size(480, 492);
+            this.DgvListRight.TabIndex = 93;
+            // 
+            // FrmOperators
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 589);
+            this.ControlBox = false;
+            this.Controls.Add(this.DgvListRight);
+            this.Controls.Add(this.PnlBtn);
+            this.Controls.Add(this.DgvListLeft);
+            this.Name = "FrmOperators";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "担当者を決定";
+            this.Load += new System.EventHandler(this.FrmOperators_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvListLeft)).EndInit();
+            this.PnlBtn.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvListRight)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvListLeft;
+        private System.Windows.Forms.Panel PnlBtn;
+        private System.Windows.Forms.Button BtnAssign;
+        private System.Windows.Forms.Button BtnClose;
+        private System.Windows.Forms.DataGridView DgvListRight;
+    }
+}
